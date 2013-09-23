e63f6210bd7b8c834c6b42ac8f813101ff0c3ebb
ruslan <ruslan.albu@hotmail.com>
2013-09-23 22:36:56 +0100

    db added

---
+++ AspNetWithAngular/Data/MessageBoardMigrationsConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+
+namespace AspNetWithAngular.Data
+{
+	public class MessageBoardMigrationsConfiguration : DbMigrationsConfiguration<MessageBoardContext>
+	{
+		public MessageBoardMigrationsConfiguration()
+		{
+			this.AutomaticMigrationDataLossAllowed = true;
+			AutomaticMigrationsEnabled = true;
+		}
+
+		protected override void Seed(MessageBoardContext context)
+		{
+			base.Seed(context);
+
+#if	DEBUG
+			if (context.Topics.Count() == 0)
+			{
+				var topic = new Topic
+					{
+						Title = "Title",
+						Created = DateTime.Now,
+						Body = "Some body",
+						Replies = new List<Reply>
+							{
+								new Reply
+									{
+										Body = "first reply",
+										Created = DateTime.Now
+									},
+								new Reply
+									{
+										Body = "second reply",
+										Created = DateTime.Now
+									}
+							}
+					};
+
+				context.Topics.Add(topic);
+			}
+#endif
+
+		}
+	}
+}
---
+++ AspNetWithAngular/Data/Topic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWithAngular.Data
+{
+	public class Topic
+	{
+		public string Title { get; set; }
+		public string Body { get; set; }
+		public DateTime Created { get; set; }
+		public int Id { get; set; }
+		public bool Flagged { get; set; }
+
+		public ICollection<Reply> Replies { get; set; }
+	}
+}
