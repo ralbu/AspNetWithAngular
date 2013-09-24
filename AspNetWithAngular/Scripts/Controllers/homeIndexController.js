f677555dfe978ba0902f9ff58503c818c4b318f6
ruslan <ruslan.albu@hotmail.com>
2013-09-24 17:49:42 +0100

    hardcoded data for angular

--- AspNetWithAngular/Scripts/Controllers/homeIndexController.js
+++ AspNetWithAngular/Scripts/Controllers/homeIndexController.js
@@ -1,3 +1,22 @@
-﻿function homeIndexController() {
-    alert('con');
+﻿function homeIndexController($scope) {
+    $scope.name = "john";
+    $scope.data = [
+        {
+            title: "some title",
+            body: "a body",
+            created: "02/05/2013"
+        }, {
+            title: "some second title",
+            body: "a body",
+            created: "02/05/2013"
+        }, {
+            title: "some fird title",
+            body: "a body",
+            created: "02/05/2013"
+        }, {
+            title: "some forth title",
+            body: "a body",
+            created: "02/05/2013"
+        }
+    ];
 }
